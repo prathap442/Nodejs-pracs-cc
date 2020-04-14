22514161f42bdd14950da4e86f303c01445eac75
prathap442 <pmohansaikrishna@gmail.com>
2020-04-14 10:35:33 +0530

    added some modifications for the exporting of variables from one file to other

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 // require here is nothing but the function to which we pass the arguments 
 // the arguments are the greet which is the file name in the current directory
-require("./greet");
+var greet = require("./greet");
+console.log(greet());
--- greet.js
+++ greet.js
@@ -5,3 +5,6 @@ greet()
 // module here what ever we use comes from the core of the javascript modules
 // i.e., open the javascript node code  and see the there exist the core directory under which we can utilise 
 // certain things
+
+
+module.exports = greet;

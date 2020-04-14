e2551945fb2440a9b7a58c7a834f9c3f80bd9bfb
prathap442 <pmohansaikrishna@gmail.com>
2020-04-14 10:33:11 +0530

    require: keyword understanding

---
+++ app.js
@@ -0,0 +1,3 @@
+// require here is nothing but the function to which we pass the arguments 
+// the arguments are the greet which is the file name in the current directory
+require("./greet");
---
+++ greet.js
@@ -0,0 +1,7 @@
+var greet = function(){
+  console.log("This is the greet");
+}
+greet()
+// module here what ever we use comes from the core of the javascript modules
+// i.e., open the javascript node code  and see the there exist the core directory under which we can utilise 
+// certain things
